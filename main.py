455c31d4869ac2860c99f554389cecacf7d6ee26
kimgr1221 <92447650+kimgr1221@users.noreply.github.com>
2024-12-08 21:29:14 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 #pip install langchain-openai
 #pip install streamlit
 
-#from dotenv import load_dotenv
-#load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 
 chat_model = ChatOpenAI()
 
